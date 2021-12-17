37708a88619abee8490130f69d842ee46e006488
RoiTang <tangmeng@mail.ustc.edu.cn>
2021-12-17 19:40:35 +0800

    add vgg19

--- TestMemcap/test.py
+++ TestMemcap/test.py
@@ -22,14 +22,14 @@ resnext50_32x4d = models.resnext50_32x4d(pretrained=False)
 wide_resnet50_2 = models.wide_resnet50_2(pretrained=False)
 mnasnet = models.mnasnet1_0(pretrained=False)
 
-model = resnet18
+model = alexnet
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if device == 'cuda':
     model = model.to(device)
     model = torch.nn.DataParallel(model)
 
 summary(model, (3, 224, 224))
-# print(model)
+print(model)
 '''
 trans = []
 trans.append(transforms.Resize(size=224))
